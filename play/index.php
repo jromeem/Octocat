a837942130f45a880979523b59796599fcc9f07e
Michael Pace <mpace1027@gmail.com>
2012-10-20 08:12:28 -0500

    blahhhh

--- play/index.php
+++ play/index.php
@@ -1,4 +1,10 @@
-<?php session_start(); ?>
+<?php
+	session_start();
+
+	$user_id  = $_SESSION['user_id'];
+	$username = $_SESSION['username'];
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
