c5a8c0fc4ce6400ad471ddc10b335e0b08f7cd47
Michael Pace <mpace1027@gmail.com>
2012-10-20 03:33:02 -0500

    added more images

--- helper.php
+++ helper.php
@@ -1,27 +1,11 @@
 <?php
-function test(){
-	return "daowzadsfaf";
-}
-
-function databaseAccess(){
-
-try {
-$conn = new PDO ( "sqlsrv:server = tcp:w6wekl8y64.database.windows.net,1433; Database = octocat_db", "octocat", "Hacktx12");
-$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch ( PDOException $e ) {
-print( "Error connecting to SQL Server." );
-die(print_r($e));
-}
-
-#SQL Server Extension Sample Code:
-
-$connectionInfo = array("UID" => "octocat@w6wekl8y64", "pwd" => "Hacktx12", "Database" => "octocat_db", "LoginTimeout" => 30, "Encrypt" => 1);
-$serverName = "tcp:w6wekl8y64.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-return $conn;
-
-}
-
+	$host = "mysql6.000webhost.com";
+	 $mysql_database = "a4259366_octo";
+	 $mysql_user = "a4259366_octo";
+	 $mysql_password = "asdf1234";
+	 
+	 mysql_connect($mysql_host, $mysql_user, $mysql_password);
+	 mysql_select_db($mysql_database);
+	 $result = mysql_query("SELECT * FROM teletub_users");
+	echo $result;
 ?>
