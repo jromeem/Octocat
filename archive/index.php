a4959c203d9dc4c9d6ebac7c0e29940bca2bd987
Jerome Martinez <jerome.e.martinez@gmail.com>
2012-10-20 11:30:59 -0500

    archive

--- archive/index.php
+++ archive/index.php
@@ -2,23 +2,21 @@
 
 include '../helper.php';
 
-$allGamesQuery = "SELECT *";
+$allGamesQuery = "SELECT * FROM games WHERE game_over='1'";
 
 $allGames = connectAndRead($allGamesQuery);
+echo "size: " + count($allGames);
 
 // get the word array
 for($i=0; $i<=count($allGames); $i++){
-	echo "going through all games"+i;
-	if($allGames[i][game_over] == 1){
 		echo "game is over";
 		$wordsArray=$allGames[i][words];
 		$drawsArray=$allGames[i][draws];
 		//display connect to index.html
 		for($j=0; $j<=count($wordsArray); $j++){
-			echo wordsArray[j];
-			echo drawsArray[j];
+			echo wordsArray[$j];
+			echo drawsArray[$j];//canvas
 		}
-	}
 }
 
 $phrase_json = json_encode(array(0 => $phrase));
