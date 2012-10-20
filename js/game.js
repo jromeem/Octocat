68c5fe62e7bd5787526855aba3ceef26e543ab3e
Michael Pace <mpace1027@gmail.com>
2012-10-19 21:38:39 -0500

    style and game.js updates

--- js/game.js
+++ js/game.js
@@ -1,2 +1,16 @@
 $(document).ready(function(){
+	var canvasDiv = document.getElementById('canvasDiv');
+	canvas = document.createElement('canvas');
+	canvas.setAttribute('width', 400);
+	canvas.setAttribute('height', 400);
+	canvas.setAttribute('id', 'canvas');
+	canvasDiv.appendChild(canvas);
+	if(typeof G_vmlCanvasManager != 'undefined') {
+		canvas = G_vmlCanvasManager.initElement(canvas);
+	}
+	context = canvas.getContext("2d");
+
+	$('#canvas').mousedown(function(e){
+		alert("hi");
+	});
 });
