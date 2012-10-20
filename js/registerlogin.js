2da5f2c22477d89c19755d1d2c9f148da8d97955
Michael Pace <mpace1027@gmail.com>
2012-10-20 06:06:19 -0500

    login frontent

--- js/registerlogin.js
+++ js/registerlogin.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	// register
 	$('#register').click(function(){
 		$('#registerPopup').show();
 	});
@@ -12,6 +13,26 @@ $(document).ready(function(){
 				}
 				else{
 					$('body').html('<h1>GOOD JOB, YOU REGISTERED</h1>');
+				}
+			});
+			return false;
+		}
+	});
+
+	// login
+	$('#login').click(function(){
+		$('#loginPopup').show();
+	});
+
+	$('#login-username').keypress(function(e){
+		if (e.keyCode == 13){
+			var data = { username : $('#login-username').val() };
+			$.post('login_action.php', data, function(result) {
+				if (result['status'] == 'failure'){
+					$('body').html('<h1>LOGIN FAILURE, TOO BAD</h1>');
+				}
+				else{
+					$('body').html('<h1>GOOD JOB, YOU\'RE LOGGED IN</h1>');
 				}
 			});
 			return false;
