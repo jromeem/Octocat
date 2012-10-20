efdcdfb9ce37fffe365d763c1eb2b4e36080fd45
Michael Pace <mpace1027@gmail.com>
2012-10-20 06:02:07 -0500

    registration is perfect

--- js/registerlogin.js
+++ js/registerlogin.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
 		if (e.keyCode == 13){
 			var data = { username : $('#register-username').val() };
 			$.post('register_action.php', data, function(result) {
-				console.log(result);
+				if (result['status'] == 'failure'){
+					$('body').html('<h1>NAME ALREADY TAKEN, TOO BAD</h1>');
+				}
+				else{
+					$('body').html('<h1>GOOD JOB, YOU REGISTERED</h1>');
+				}
 			});
 			return false;
 		}
