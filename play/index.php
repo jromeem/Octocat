037efb1e8808e4e52266065e64e2bacdff300cb6
Daowz <sriratana.s@gmail.com>
2012-10-20 04:46:22 -0500

    style

--- play/index.php
+++ play/index.php
@@ -3,12 +3,31 @@
 	<head>
 		<script src="../js/jquery-1.8.2.min.js"></script>
 		<script src="../js/game.js"></script>
-		<link rel="stylesheet" type="text/css" href="../css/style.css" />
 		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="../css/style.css" />
 	</head>
 	<body>
-		<div id="header"></div>
-		<div id="content">
+        <div class="container">
+		<div id="header">
+            <table>
+            	<tr>
+                <td>
+        			<div id = "logo">
+						<img src="../img/octomom_logo.png" id="logo"/>
+        			</div>
+                </td>
+                <td>   
+           			<div class = "topbar">
+                    <ul>
+                    		<li><img src="../img/archive.png" id="menuIcon"/></li>
+                        	<li><img src="../img/logout.png" id="menulogout"/></li>
+                    	</ul>
+            		</div>
+                </td>
+                </tr>
+           </table>
+		</div>
+		<div >
 			<div id="gameDiv">
 				<div id="canvasDiv">
 					<button id="clear">Clear!</button>
@@ -22,5 +41,6 @@
 			<!-- <a href="next page"><img src="trashhhh" id="createButton" /></a>
 			<a href="other next page"><img src="other trash" id="playButton" /></a> -->
 		</div>
+        </div>
 	</body>
 </html>
