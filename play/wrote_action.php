2f29f7766f52e7d6d8f908b66620c0ca3059012e
Jerome Martinez <jerome.e.martinez@gmail.com>
2012-10-20 12:33:16 -0500

    revert wrote and drew actions

--- play/wrote_action.php
+++ play/wrote_action.php
@@ -2,58 +2,16 @@
 
 include '../helper.php';
 
-$game_id = $_SESSION['game_id'];
+$gameId = $_SESSION['game_id'];
 
 $phrase = $_POST['phrase'];
 
-// find if there were previous words already
-$gameStackQuery = "SELECT words FROM games WHERE id=$game_id";
-$gameStack = connectAndRead($gameStackQuery);
+$phraseJson = json_encode(array($phrase));
 
-// if there were previous words
-if (!empty($gameStack)) {
-	echo "gameStack: ";
-	echo gettype($gameStack);
-	print_r($gameStack);
-	echo "<br/>";
+$phraseQuery = "UPDATE games SET words='$phraseJson' WHERE id=$gameId";
 
-	$stackArr = json_decode($gameStack[0]);
-	
-	echo "stackArr: ";
-	echo gettype($stackArr);
-	print_r($stackArr);
-	echo "<br/>";
-	
-	$newStack = array_push($stackArr, $phrase);
-	
-	echo "newStack: ";
-	echo gettype($newStack);
-	print_r($newStack);
-	echo "<br/>";
-	
-	$newStackJson = json_encode($newStack);
-	
-	echo "newStackJson: ";
-	echo gettype($newStackJson);
-	print_r($newStackJson);
-	echo "<br/>";
-	
-	$wordQuery = "UPDATE games SET words='$newStackJson' WHERE id=$gameId";
-	
-	echo "newStackJson: ";
-	echo gettype($wordQuery);
-	print_r($wordQuery);
-	echo "<br/>";
-	
-	connectAndWrite($wordQuery);
-	
-// this is the first word
-} else {
-	echo "was the first draw";
+connectAndWrite($phraseQuery);
 
-	$phrase_json = json_encode(array($phrase));
-	$phraseQuery = "UPDATE games SET words='$phrase_json' WHERE id=$game_id";
-	connectAndWrite($phraseQuery);
 }
 
 ?>
