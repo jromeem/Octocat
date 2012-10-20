23c3699b84c300cea018ed5c48242fb2619d85b2
Daowz <sriratana.s@gmail.com>
2012-10-20 01:24:40 -0500

    login

--- createGame.php
+++ createGame.php
@@ -8,9 +8,16 @@
 	$creator = '';
 	$word[0] = $_POST["wordInput"];	
 	
+	
 	echo $word[0];
 	
 	echo $con;
+	
+	mysql_select_db("my_db", $con);
+	
+	mysql_query("INSERT INTO User (ID, Name, Email Address)
+VALUES (1, 'test','test@')");
+
 	//go int ocreat row in db of table 
 
 ?>
