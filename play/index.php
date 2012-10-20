eea628bf1680998bfbf1e19872df5e3d0fa6b0bc
Jerome Martinez <jerome.e.martinez@gmail.com>
2012-10-20 08:39:21 -0500

    play action sldkjsdkfjl

--- play/index.php
+++ play/index.php
@@ -1,4 +1,19 @@
-<?php session_start(); ?>
+<?php session_start(); 
+
+	incude '../helper.php';
+	
+	$rand_val = rand(1, 10);
+	$query = 'SELECT * FROM games WHERE id='.$rand_val;
+	$result = connectAndRead($query);
+	
+	$play = '';
+	if ($_SESSION['turn'] == 'words') {
+		$play = json_decode($result[0]['words']);
+	} else {
+		$play = json_decode($result[0]['draws']);
+	}
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
@@ -30,10 +45,15 @@
 		</div>
 		<div >
 			<div id="gameDiv">
-				<div id="canvasDiv">
-					<button id="clear">Clear!</button>
-					<button id="save">Save!</button>
-				</div>
+				<?php
+					if ($_SESSION['turn'] == 'words') {
+						echo "<h1>$play</h1>";
+					} else {
+						echo '<div id="canvasDiv">
+							  <button id="clear">Clear!</button>
+							  <button id="save">Save!</button></div>';
+					}
+				?>
 				<div id="wordDiv">
 					WAT IS THIS, <?= $_SESSION['username']; ?>?????? YOUR USER ID IS <?= $_SESSION['user_id']; ?>
 					<input type="text" id="wordInput" />
