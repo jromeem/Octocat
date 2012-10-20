83791dbf708634a289f17b1aa08876f99bdc7716
Michael Pace <mpace1027@gmail.com>
2012-10-20 04:13:19 -0500

    more organization

---
+++ create/index_submit.php
@@ -0,0 +1,18 @@
+<?php
+    include 'helper.php';
+	
+	$query = 'SELECT * FROM users';
+    $result = connectAndQuery($query);
+	
+	//print_r($result);
+	echo $result[0]['username'];
+	
+	$creator = '';
+	$word[0] = $_POST["wordInput"];	
+	
+	echo $word[0];
+	
+	print_r($con)
+	//go int ocreat row in db of table 
+
+?>
